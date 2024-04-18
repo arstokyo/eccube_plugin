c820efbfea2b85dfa3d3106d01528507bde704d1
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-18 15:23:24 +0900

    test serialize

---
+++ app/Plugin/AceClient/AceServices/Model/Request/Jyuden/AddCart/NmemModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Plugin\AceClient\AceServices\Model\Request\Jyuden\AddCart;
+
+use Plugin\AceClient\AceServices\Model\Request\Jyuden\Dependency\NmemModelAbstract;
+use Plugin\AceClient\AceServices\Model\Request\Jyuden\Dependency\NmemModelInterface;
+
+class NmemModel extends NmemModelAbstract implements NmemModelInterface
+{
+    
+}
