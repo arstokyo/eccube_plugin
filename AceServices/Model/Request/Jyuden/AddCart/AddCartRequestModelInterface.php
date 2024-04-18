c75d3477ab392762b3bd083544dd48e2c87dd67d
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-18 09:57:24 +0900

    test serialize

--- AceServices/Model/Request/Jyuden/AddCart/AddCartRequestModelInterface.php
+++ AceServices/Model/Request/Jyuden/AddCart/AddCartRequestModelInterface.php
@@ -2,5 +2,10 @@
 
 namespace Plugin\AceClient\AceServices\Model\Request\Jyuden\AddCart;
 
-use Plugin\AceClient\AceServices\Model\Request;
+use Plugin\AceClient\AceServices\Model\Request\RequestModelInterface;
 use Plugin\AceClient\AceServices\Model\Request\Jyuden\JyudenRequestInterface;
+
+interface AddCartRequestModelInterface extends RequestModelInterface , JyudenRequestInterface
+{
+
+}
