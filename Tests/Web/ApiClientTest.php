6446d4b03bf18ef86e403d359aa32388d4e28859
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-19 17:56:49 +0900

    test serialize

--- Tests/Web/ApiClientTest.php
+++ Tests/Web/ApiClientTest.php
@@ -183,7 +183,7 @@ class ApiClientTest extends AbstractAdminWebTestCase
         $extension->load([], $container);
 
         // Get the soap_serializer service
-        $soapSerializer = $container->getParameter('soap_xml_serializer');
+        $soapSerializer = $container->getParameter('soap_xml_serializer.XMLNS');
         
         var_dump($soapSerializer);
         
