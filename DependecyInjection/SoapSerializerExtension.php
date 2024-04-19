7ff26d572800ab3b07728aae46cefb04c14aed95
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-19 16:45:04 +0900

    test serialize

--- DependecyInjection/SoapSerializerExtension.php
+++ DependecyInjection/SoapSerializerExtension.php
@@ -13,10 +13,10 @@ class SoapSerializerExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new SoapSerializerConfiguration();
+        $processedConfig = $this->processConfiguration($configuration, $configs);
         
         $loader = new YamlFileLoader($container, new FileLocator(ConfigFileMapper::ROOT_CONFIG_PATH));
         $loader->load(ConfigFileMapper::SOAP_SERIALIZER_FILE_NAME);
-        $processedConfig = $this->processConfiguration($configuration, $configs);
         
         $soapSerializerDefinition = $container->getDefinition('ace_client.soap_serializer');
         // Your code here
