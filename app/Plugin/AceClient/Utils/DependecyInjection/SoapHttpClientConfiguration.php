18dfe3d6e22a2eade8c5e17dbbc73445bd7ed2eb
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-19 14:53:30 +0900

    test serialize

---
+++ app/Plugin/AceClient/Utils/DependecyInjection/SoapHttpClientConfiguration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Plugin\AceClient\Utils\DependecyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class SoapHttpClientConfiguration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('SoapHttpClient');
+        $rootNode = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->scalarNode('wsdl')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+                ->scalarNode('options')
+                    ->defaultValue([])
+                ->end()
+            ->end();
+        return $treeBuilder;
+    }
+}
