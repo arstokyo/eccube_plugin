76a09f6d42409a94abec0b880966d66f7c7d1ee7
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-19 18:23:15 +0900

    test serialize

--- DependecyInjection/AceClientExtension.php
+++ DependecyInjection/AceClientExtension.php
@@ -8,18 +8,11 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class SoapXmlSerializerExtension extends Extension
+class AceClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        // $configuration = new SoapXmlSerializerConfiguration();
-        // $processedConfig = $this->processConfiguration($configuration, $configs);
-        
         $loader = new YamlFileLoader($container, new FileLocator(ConfigFileMapper::ROOT_CONFIG_PATH));
         $loader->load(ConfigFileMapper::SOAP_SERIALIZER_FILE_NAME);
-
-        // $container->setParameter('soap_serializer.XMLNS', $processedConfig['XMLNS']);
-        
-        // Your code here
     }
 }
