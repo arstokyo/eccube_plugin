c905e84274777f4082a16d79debae280fa0bfb71
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-20 14:56:33 +0900

    testLoadPrmDTOFormat

--- Utils/Denormalize/OTDXmlDenormalizer.php
+++ Utils/Denormalize/OTDXmlDenormalizer.php
@@ -8,20 +8,20 @@ class OTDXmlDenormalizer extends OTDDenormalizerAbstract
 
     public function denormalizeOTD(OTDDelegateInterface $delegate): mixed
     {
-        // Get the object and options from the delegate
-        $object = $delegate->getObject();
-        $options = $delegate->getDenomarlizeOptions();
+        // // Get the object and options from the delegate
+        // $object = $delegate->getObject();
+        // $options = $delegate->getDenomarlizeOptions();
 
-        // Get the XML data from the object
-        $xml = $object->getXml();
+        // // Get the XML data from the object
+        // $xml = $object->getXml();
 
-        // Create a new SimpleXMLElement object from the XML data
-        $xmlElement = new \SimpleXMLElement($xml);
+        // // Create a new SimpleXMLElement object from the XML data
+        // $xmlElement = new \SimpleXMLElement($xml);
 
-        // Denormalize the XML data into the object
-        $this->denormalizeXmlElement($xmlElement, $object, $options);
+        // // Denormalize the XML data into the object
+        // $this->denormalizeXmlElement($xmlElement, $object, $options);
 
-        return $object;
+        // return $object;
     }
     // Add your code here
 }
