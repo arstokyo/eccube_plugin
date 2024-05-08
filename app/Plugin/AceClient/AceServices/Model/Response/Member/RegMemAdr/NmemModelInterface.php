5d289755270639c965d4ecc552753c42006e42fd
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-08 12:05:43 +0900

    Modify Response/Member/RegMemAdr/NmemModel

--- app/Plugin/AceClient/AceServices/Model/Response/Member/RegMemAdr/NmemModelInterface.php
+++ app/Plugin/AceClient/AceServices/Model/Response/Member/RegMemAdr/NmemModelInterface.php
@@ -3,28 +3,14 @@
 namespace Plugin\AceClient\AceServices\Model\Response\Member\RegMemAdr;
 
 use Plugin\AceClient\AceServices\Model\Dependency\Person\NmemGroup1Interface;
+use Plugin\AceClient\AceServices\Model\Dependency\NoCategory\HasBetuInterface;
 
 /**
  * Interface for Nmem Model
  * 
  * @author Ars-Thong <v.t.nguyen@ar-system.co.jp>
  */
-interface NmemModelInterface extends NmemGroup1Interface
+interface NmemModelInterface extends NmemGroup1Interface, HasBetuInterface
 {
-    
-    /**
-     * Get 住所区分
-     * 
-     * @return int|null
-     */
-    public function getBetu(): ?int;
-
-    /**
-     * Set 住所区分
-     * 
-     * @param int|null $betu
-     * @return self
-     */
-    public function setBetu(?int $betu): self;
 
 }
