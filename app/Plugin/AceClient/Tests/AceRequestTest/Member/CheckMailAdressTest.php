27535512ee4d0a9838f47ab6555c70fa8c402f19
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-08 20:58:05 +0900

    Modify Tests\AceRequestTest\Membe\CheckMailAdressRequestModelTest

--- app/Plugin/AceClient/Tests/AceRequestTest/Member/CheckMailAdressTest.php
+++ app/Plugin/AceClient/Tests/AceRequestTest/Member/CheckMailAdressTest.php
@@ -7,20 +7,22 @@ use Plugin\AceClient\AceServices\Model\Request\Member\CheckMailAdress\CheckMailA
 use Plugin\AceClient\AceServices\Model\Response\Member\CheckMailAdress\CheckMailAdressResponseModel;
 use Plugin\AceClient\AceClient;
 use GuzzleHttp\Exception\ClientException;
+use Plugin\AceClient\Utils\Mapper\OverviewMapper;
 
 class CheckMailAdressRequestModelTest extends AbstractAdminWebTestCase
 {
+    private ?string $checkMail = 'CheckMailAdressTest@AceClient.v.1.0';
     public function testCalCheckMailAdressRequestModel()
     {
         $mail = new CheckMailAdressRequestModel();
-        $mail->setId(0)->setMailAdress("1234");
-        $this->assertEquals(0, $mail->getId());
-        $this->assertEquals(1234, $mail->getMailAdress());
+        $mail->setId(OverviewMapper::ACE_TEST_SYID)->setMailAdress($this->checkMail);
+        $this->assertEquals(OverviewMapper::ACE_TEST_SYID, $mail->getId());
+        $this->assertEquals($this->checkMail, $mail->getMailAdress());
     }
-    public function testRequestCheckMailAdressOK()
+    public function testRequestCheckMailAdressCase1()
     {
         try {
-            $getCheckMailAdressRequest = $this->getCheckMailAdressRequestModelOK();
+            $getCheckMailAdressRequest = $this->getCheckMailAdressRequestModelCase1();
             $response = (new AceClient)->makeMemberService()
                                        ->makeCheckMailAdressMethod()
                                        ->withRequest($getCheckMailAdressRequest)
@@ -42,15 +44,53 @@ class CheckMailAdressRequestModelTest extends AbstractAdminWebTestCase
         } catch(\Throwable $e) {
             $message1 = $e->getMessage() ?? 'One Error Occurred when sending request.';
         }
-        $this->assertEquals('abc', $mailAdress);
+        $this->assertEquals($this->checkMail, $mailAdress);
+        $this->assertEquals('NG', $result);
+        $this->assertEquals('メールアドレスの登録がありますのでお問合せか、お電話にてご連絡ください。', $message1);
+        $this->assertEquals('select mw.mbid from memweb mw inner join member mb on mb.syid=mw.syid and mb.mbid=mw.mbid and mb.delfg=0 where mw.syid=:xid and (mw.userid=:xmail or mw.email=:xmail) and status=0', $message2);
+    }
+    public function getCheckMailAdressRequestModelCase1()
+    {
+        $mail = new CheckMailAdressRequestModel();
+        $mail->setId(OverviewMapper::ACE_TEST_SYID)->setMailAdress($this->checkMail);
+        return $mail;
+    }
+
+    public function testRequestCheckMailAdressCase2()
+    {
+        try {
+            $getCheckMailAdressRequest = $this->getCheckMailAdressRequestModelCase2();
+            $response = (new AceClient)->makeMemberService()
+                                       ->makeCheckMailAdressMethod()
+                                       ->withRequest($getCheckMailAdressRequest)
+                                       ->send();
+            if ($response->getStatusCode() === 200) {
+                /** @var CheckMailAdressResponseModel $responseObj */
+                $responseObj = $response->getResponse();
+                $mailAdress = $responseObj->getMember()
+                                        ->getMessage()
+                                        ->getAdress();
+                $result = $responseObj->getMember()
+                                        ->getMessage()
+                                        ->getResult();
+                $message1 = $responseObj->getMember()->getMessage()->getMessage1() ?? null;
+                $message2 = $responseObj->getMember()->getMessage()->getMessage2() ?? null;
+            }
+        } catch(ClientException $e) {
+            $message1 = $e->getMessage() ?? 'One Error Occurred when sending request.';
+        } catch(\Throwable $e) {
+            $message1 = $e->getMessage() ?? 'One Error Occurred when sending request.';
+        }
+        $this->assertEquals('do-not-exist-this-email@AceClient.v.1.0', $mailAdress);
         $this->assertEquals('OK', $result);
         $this->assertEquals('', $message1);
         $this->assertEquals('', $message2);
     }
-    public function getCheckMailAdressRequestModelOK()
+
+    public function getCheckMailAdressRequestModelCase2()
     {
         $mail = new CheckMailAdressRequestModel();
-        $mail->setId(7)->setMailAdress('abc');
+        $mail->setId(OverviewMapper::ACE_TEST_SYID)->setMailAdress('do-not-exist-this-email@AceClient.v.1.0');
         return $mail;
     }
 }
