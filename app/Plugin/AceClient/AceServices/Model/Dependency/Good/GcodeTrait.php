4c0679233ee3c7a5841c44f825cc042998956217
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-10 17:18:40 +0900

    Add Model\Dependency\Good\Gcode|Tanka

---
+++ app/Plugin/AceClient/AceServices/Model/Dependency/Good/GcodeTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Plugin\AceClient\AceServices\Model\Dependency\Good;
+
+/*
+ * Trait for 商品コード
+ * 
+ * @author Ars-Thong <v.t.nguyen@ar-system.co.jp>
+ */
+trait GcodeTrait
+{
+    
+    /** @var string $gcode 商品コード */
+    protected ?string $gcode = null;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getGcode(): ?string
+    {
+        return $this->gcode;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setGcode(?string $gcode)
+    {
+        $this->gcode = $gcode;
+        return $this;
+    }
+}
