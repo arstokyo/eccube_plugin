19e7f0fd4c7f6343f6816b961e2d7ee413d2c2cb
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-13 22:09:53 +0900

    Add AceDateTimeTest, Modify AceDateTime

--- app/Plugin/AceClient/Utils/Normalize/AceDateTimeNormalizer.php
+++ app/Plugin/AceClient/Utils/Normalize/AceDateTimeNormalizer.php
@@ -5,6 +5,11 @@ namespace Plugin\AceClient\Utils\Normalize;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Plugin\AceClient\AceServices\Model\CustomDataType\AceDateTime\AceDateTime;
 
+/**
+ * Normalizer for AceDateTime
+ * 
+ * @author Ars-Thong <v.t.nguyen@ar-system.co.jp>
+ */
 class AceDateTimeNormalizer implements NormalizerInterface
 {
     public function normalize($object, string $format = null, array $context = [])
