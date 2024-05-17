f6d6cadbb93ac909a5e794970ca5c6d3a8a246c1
phuoclm <leminhphuoc04@gmail.com>
2024-05-17 18:40:18 +0900

    Modify Model\Dependency\Jyudens\Jyumei

--- app/Plugin/AceClient/AceServices/Model/Dependency/Jyudens/Jyumei/JyumeiModelGroup1Interface.php
+++ app/Plugin/AceClient/AceServices/Model/Dependency/Jyudens/Jyumei/JyumeiModelGroup1Interface.php
@@ -2,11 +2,8 @@
 
 namespace Plugin\AceClient\AceServices\Model\Dependency\Jyudens\Jyumei;
 
-use Plugin\AceClient\AceServices\Model\Dependency\Good;
-use Plugin\AceClient\AceServices\Model\Dependency\NoCategory;
 use Plugin\AceClient\AceServices\Model\Dependency\Cost;
 use Plugin\AceClient\AceServices\Model\Dependency\Zaiko;
-use Plugin\AceClient\AceServices\Model\Dependency\Bikou;
 
 
 /**
@@ -14,18 +11,9 @@ use Plugin\AceClient\AceServices\Model\Dependency\Bikou;
  *
  * @author Ars-Phuoc <m.phuoc.le@ar-system.co.jp>
  */
-interface JyumeiModelGroup1Interface extends Good\HasGcodeInterface,
-                                             NoCategory\HasSuuInterface,
-                                             Cost\Tanka\HasTankaInterface,
+interface JyumeiModelGroup1Interface extends JyumeiModelBaseInterface,
                                              Zaiko\HasIgnoreZaikoInterface,
                                              Zaiko\HasChoseiZaikoInterface,
-                                             Bikou\HasMBikouInterface,
-                                             Cost\Teika\HasTeikaInterface,
-                                             Cost\HasRituInterface,
-                                             Cost\Money\HasMoneyInterface,
-                                             Cost\Tax\HasTaxKbnInterface,
-                                             Cost\Genka\HasGenkaInterface,
-                                             Good\HasKpkbnInterface
+                                             Cost\Tax\HasTaxKbnInterface
 {
-
 }
