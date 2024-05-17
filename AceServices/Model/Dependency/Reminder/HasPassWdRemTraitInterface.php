55b9695f5dfc37281c4dd6173d07d647c90e4e7d
kmorino <k-morino@ar-system.co.jp>
2024-05-17 23:27:16 +0900

    [WIP] Add Dependency\Reminder\PassWdRem

---
+++ AceServices/Model/Dependency/Reminder/HasPassWdRemTraitInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plugin\AceClient\AceServices\Model\Dependency\Reminder;
+
+/**
+ * Interface for Has PassWdRemInterface
+ *
+ * @author kmorino
+ */
+interface HasPassWdRemTraitInterfaceextends extends HasQuestionInterface, HasAnswerInterface
+{
+
+}
