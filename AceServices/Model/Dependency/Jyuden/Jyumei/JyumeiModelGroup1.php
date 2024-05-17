a6a97371f1bbf8531fe40adda76dafa4b1675661
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-17 16:47:20 +0900

    Add Jyumei\JyumeiModelGroup1

---
+++ AceServices/Model/Dependency/Jyuden/Jyumei/JyumeiModelGroup1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plugin\AceClient\AceServices\Model\Dependency\Jyuden\Jyumei;
+
+/**
+ * Model for Jyumei Group1
+ * 
+ * @author Ars-Phuoc <>
+ */
+class JyumeiModelGroup1 implements JyumeiModelGroup1Interface
+{
+
+}
---
+++ AceServices/Model/Dependency/Jyuden/Jyumei/JyumeiModelGroup1Interface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plugin\AceClient\AceServices\Model\Dependency\Jyuden\Jyumei;
+
+/**
+ * Interface for JyumeiModelGroup1
+ *
+ * @author Ars-Phuoc <>
+ */
+interface JyumeiModelGroup1Interface
+{
+    
+}
