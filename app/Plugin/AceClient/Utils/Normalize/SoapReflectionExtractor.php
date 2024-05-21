063592cf7ff863598b6a893f06dfe2d71dd2209b
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-21 19:02:20 +0900

    Modify SoapReflectionExtractor

--- app/Plugin/AceClient/Utils/Normalize/SoapReflectionExtractor.php
+++ app/Plugin/AceClient/Utils/Normalize/SoapReflectionExtractor.php
@@ -3,6 +3,7 @@
 namespace Plugin\AceClient\Utils\Normalize;
 
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\Type;
 
 /**
  * Class for Soap Reflection Extractor
@@ -11,6 +12,12 @@ use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
  */
 class SoapReflectionExtractor extends ReflectionExtractor
 {
+
+    const MAP_TYPES = [
+        'integer' => Type::BUILTIN_TYPE_INT,
+        'boolean' => Type::BUILTIN_TYPE_BOOL,
+        'double' => Type::BUILTIN_TYPE_FLOAT,
+    ];
 
     /**
      * {@inheritDoc}
