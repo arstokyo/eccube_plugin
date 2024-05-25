d694320ef580516e683cdc148399d7b33b18b74b
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-25 17:44:08 +0900

    Modify ConfigWriter, ConfigType, ConfigController

--- Utils/ConfigWriter/ConfigWriter.php
+++ Utils/ConfigWriter/ConfigWriter.php
@@ -5,23 +5,26 @@ namespace Plugin\AceClient\Utils\ConfigWriter;
 use Symfony\Component\Yaml\Yaml;
 use Plugin\AceClient\Utils\Mapper\FilePathMapper;
 use Plugin\AceClient\Utils\Mapper\ConfigNodeRootNameMapper;
+use Plugin\AceClient\Entity\Config;
+use Plugin\AceClient\Utils\Log\LoggerFactory;
 
 class ConfigWriter
 {
 
     /**
-     * Update base_uri in AceClientConfig.yaml
+     * Update AceClientConfig.yaml
      *
-     * @param string $newUri
+     * @param Config $config
      * @return bool
      * @author Ars-Charan, Ars-Thong
      */
-    public static function updateBaseUri(string $newUri): bool
+    public static function updateAceClientConfig(Config $config): bool
     {
         $filePath = FilePathMapper::ROOT_CONFIG_PATH .\DIRECTORY_SEPARATOR. FilePathMapper::ACE_CLIENT_FILE_NAME;
 
         $srcConfig = Yaml::parseFile($filePath);
-        $srcConfig['parameters'][ConfigNodeRootNameMapper::ACE_METHOD]['default']['http_client']['base_uri'] = $newUri;
+        $srcConfig['parameters'][ConfigNodeRootNameMapper::ACE_METHOD]['default']['http_client']['base_uri'] = $config->getBaseUri();
+        $srcConfig['parameters'][ConfigNodeRootNameMapper::ACE_METHOD]['default']['logger']['class_name'] = $config->getIsLogOn() ? LoggerFactory::DEFAUT_LOGGER_CLASS : LoggerFactory::NULL_LOGGER_CLASS;
 
         $newYaml = Yaml::dump($srcConfig, 10, 4);
         return false !== file_put_contents($filePath, $newYaml);
