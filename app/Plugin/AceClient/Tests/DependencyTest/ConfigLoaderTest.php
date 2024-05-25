7e21fab927c77bcb76f3a1c36ddaf41ad36c9843
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-05-26 03:34:03 +0900

    Add ServiceRetrieveHelper, config\services.yaml

---
+++ app/Plugin/AceClient/Tests/DependencyTest/ConfigLoaderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Plugin\AceClient\Tests\AceRequestTest;
+
+use Eccube\Tests\Web\Admin\AbstractAdminWebTestCase;
+use Plugin\AceClient\Utils\Helper\ServiceRetrieveHelper;
+
+class ConfigLoaderTest extends AbstractAdminWebTestCase
+{
+   
+    public function testAutoWireServiceRetrieveHelper()
+    {
+        self::bootKernel();
+        $container = static::$kernel->getContainer();
+
+        /** @var ServiceRetrieveHelper $aceClientHelper */
+        $aceClientHelper = $container->get('service.retrieve.helper');
+
+        $this->assertNotNull($aceClientHelper->getConfigRepository());
+    }
+}
