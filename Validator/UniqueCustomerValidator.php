b5a1f3445b92961cbda8f746d2e49344852d36e9
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-06-07 00:56:15 +0900

    顧客の存在チェックValidatorを追加

---
+++ Validator/UniqueCustomerValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\AceClient43\Validator;
+
+use Plugin\AceClient43\Bridge\CustomerBridge;
+use Plugin\AceClient43\Exception\CouldNotCheckCustomerExistingException;
+use Plugin\AceClient43\Repository\ConfigRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class UniqueCustomerValidator extends ConstraintValidator
+{
+    private CustomerBridge $customerBridge;
+    private ConfigRepository $configRepository;
+    private TranslatorInterface $translator;
+
+    public function __construct(CustomerBridge $customerBridge, ConfigRepository $configRepository, TranslatorInterface $translator)
+    {
+        $this->customerBridge = $customerBridge;
+        $this->configRepository = $configRepository;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param string $value
+     * @param Constraint|UniqueCustomer $constraint
+     *
+     * @return void
+     *
+     * @throws CouldNotCheckCustomerExistingException
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$this->configRepository->get()->needValidateCustomerExisting()) {
+            return;
+        }
+
+        if (!$this->customerBridge->has($value)) {
+            return;
+        }
+
+        $this->context->buildViolation($this->translator->trans('ace_client.customer_existing'))
+            ->setParameter('{{ email }}', $value)
+            ->addViolation();
+    }
+}
