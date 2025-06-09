53a90eedd526752e14b5d7057a0eea01b1366a58
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-06-09 12:30:11 +0900

    お届け先編集完了イベントリスナーを追加する
    お届け先の編集完了時に顧客住所を更新するリスナーを実装する。
    エラーハンドリングを行い、ログ出力を追加する。

---
+++ EventListener/FrontMypageDeliveryEditCompleteListener.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\AceClient43\EventListener;
+
+use Eccube\Entity\CustomerAddress;
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
+use Plugin\AceClient43\Bridge\CustomerAddressBridge;
+use Plugin\AceClient43\Exception\CouldNotCreateOrUpdateCustomerAddressException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class FrontMypageDeliveryEditCompleteListener implements EventSubscriberInterface
+{
+    private CustomerAddressBridge $customerAddressBridge;
+
+    private LoggerInterface $logger;
+
+    public function __construct(CustomerAddressBridge $customerAddressBridge, LoggerInterface $logger)
+    {
+        $this->customerAddressBridge = $customerAddressBridge;
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            EccubeEvents::FRONT_MYPAGE_DELIVERY_EDIT_COMPLETE => ['onEditComplete', 100],
+        ];
+    }
+
+    /**
+     * お届け先編集完了イベントリスナー
+     *
+     * @param EventArgs $event
+     *
+     * @throws CouldNotCreateOrUpdateCustomerAddressException
+     */
+    public function onEditComplete(EventArgs $event)
+    {
+        /** @var CustomerAddress $CustomerAddress */
+        $CustomerAddress = $event->getArgument('CustomerAddress');
+        $this->logger->info('[FrontMypageDeliveryEditCompleteListener] 通販Aceの顧客住所を編集しています。', [
+            'customer_address' => $CustomerAddress,
+        ]);
+
+        try {
+            $this->customerAddressBridge->createOrUpdate($CustomerAddress, true);
+        } catch (CouldNotCreateOrUpdateCustomerAddressException $e) {
+            $this->logger->error('通販Aceの顧客住所の編集に失敗しました。', [
+                'customer_address' => $CustomerAddress,
+                'exception' => $e,
+            ]);
+            throw $e;
+        }
+    }
+}
