3794f6648ffef3978832b626547ffc35fa30e2d6
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-06-12 20:17:41 +0900

    商品作成および価格設定イベントのヘルパークラスを追加する
    商品作成時と価格設定時に使用するイベントヘルパークラスを
    新たに追加し、関連するプロパティとメソッドを定義する。

---
+++ Events/HelperOnSetPriceEvent.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\AceClient43\Events;
+
+use Eccube\Entity\Member;
+use Plugin\AceClient43\AceServices\Model\Dependency\Good\GoodTankaModelGroup1Interface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class HelperOnSetPriceEvent extends Event
+{
+    public ?OutputInterface $output = null;
+
+    public array $settingBag;
+
+    public array $options;
+
+    /** @var GoodTankaModelGroup1Interface[] */
+    public array $currentModels;
+
+    /** @var <string,GoodTankaModelGroup1Interface[]> */
+    public array $groupedTankaModels;
+
+    public GoodTankaModelGroup1Interface $firstTankaModel;
+
+    public Member $creator;
+
+    public bool $break = false;
+
+    public function __construct(
+        Member $creator,
+        array $groupedTankaModels,
+        array $currentModels,
+        GoodTankaModelGroup1Interface $firstTankaModel,
+        array $settingBag,
+        array $options,
+        ?OutputInterface $output,
+    ) {
+        $this->creator = $creator;
+        $this->groupedTankaModels = $groupedTankaModels;
+        $this->currentModels = $currentModels;
+        $this->firstTankaModel = $firstTankaModel;
+        $this->settingBag = $settingBag;
+        $this->options = $options;
+        $this->output = $output;
+    }
+}
