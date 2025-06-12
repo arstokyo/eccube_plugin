032364697919c00ef7013cbf658a7205b03520a8
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-06-12 23:22:50 +0900

    PRODUCT_IMPORT_HELPER_PRE_SET_PRICE定数を削除する
    不要なPRODUCT_IMPORT_HELPER_PRE_SET_PRICE定数を
    Events.phpから削除することで、コードを整理する。

--- Events/Events.php
+++ Events/Events.php
@@ -42,7 +42,6 @@ class Events
     public const HELPER_PRE_IMPORT_PRODUCT = 'ace_client.helper_pre_import_product';
     public const PRODUCT_IMPORT_HELPER_ON_CREATE_PRODUCT = 'ace_client.product_import_helper_on_create_product';
     public const PRODUCT_IMPORT_HELPER_ON_CREATE_PRODUCT_FAILED = 'ace_client.product_import_helper_on_create_product_failed';
-    public const PRODUCT_IMPORT_HELPER_PRE_SET_PRICE = 'ace_client.product_import_helper_pre_set_price';
     public const PRODUCT_IMPORT_HELPER_ON_SET_PRICE = 'ace_client.product_import_helper_on_set_price';
     public const POST_IMPORT_PRODUCT = 'ace_client.post_import_product';
 }
