00ad14821e66b4086442c5cd78213a1bbdd0896f
ArsPhuoc <m.phuoc.le@ar-system.co.jp>
2025-07-02 17:57:33 +0900

    [BITSUHAN_ECCUBE43-156] Aceプラグインのコンタクト連携 - Refactoring

--- Bridge/ContactBridge.php
+++ Bridge/ContactBridge.php
@@ -60,10 +60,12 @@ class ContactBridge extends BaseBridge
                             ->setNote1($content)
                     )
             );
-        $hasEventSubscribed = $this->eventDispatcher->hasListeners(Events::PRE_ADD_CONTACT);
-        if ($hasEventSubscribed) {
-            $event = new PreAddContactEvent($request, $customer, $content, $options);
-            $this->eventDispatcher->dispatch($event, Events::PRE_ADD_CONTACT);
+
+        if ($this->eventDispatcher->hasListeners(Events::PRE_ADD_CONTACT)) {
+            $this->eventDispatcher->dispatch(
+                new PreAddContactEvent($request, $customer, $content, $options),
+                Events::PRE_ADD_CONTACT
+            );
         }
 
         try {
