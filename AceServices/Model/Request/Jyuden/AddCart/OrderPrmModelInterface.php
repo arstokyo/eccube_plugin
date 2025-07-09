1d63063c8f84dea259192a863347b79d93d62f63
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-07-09 21:13:37 +0900

    [BITSUHAN_ECCUBE43-98] カート関連のイベントリスナーを追加・修正
    カートに商品を追加する際のイベントリスナーを新たに追加し、
    既存のリスナーにオプション情報の取得・設定機能を追加する。
    これにより、カート操作時の柔軟性が向上する。
    
    新規ファイル: PostAddCartListener.php
    変更ファイル: CartAddProductListener.php, OrderPrmModelInterface.php, PreAddCartListener.php

--- AceServices/Model/Request/Jyuden/AddCart/OrderPrmModelInterface.php
+++ AceServices/Model/Request/Jyuden/AddCart/OrderPrmModelInterface.php
@@ -101,4 +101,28 @@ interface OrderPrmModelInterface extends PrmModelInterface
      * @return self
      */
     public function setMailjyuden(?MailJyudenModel $mailjyuden): self;
+
+    /**
+     * Get オプション情報
+     *
+     * @return array
+     */
+    public function setOptions(array $options): self;
+
+    /**
+     * Get オプション情報
+     *
+     * @return array
+     */
+    public function getOptions(): array;
+
+    /**
+     * Add オプション情報
+     *
+     * @param string $key
+     * @param $value
+     *
+     * @return self
+     */
+    public function addOption(string $key, $value): self;
 }
