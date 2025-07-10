fd071eca5d72fcb8e09740dee82ce4e42647233c
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-07-10 19:43:43 +0900

    [BITSUHAN_ECCUBE43-98] CartSerivceのremoveProductを修正
    カートアイテムデータが渡されていない場合は、商品規格を
    使用して削除を試みる処理を実装する。商品クラスが
    インスタンスでない場合の処理も追加し、カート内の
    アイテムを比較して削除を行う。

--- Service/Decorators/CartService.php
+++ Service/Decorators/CartService.php
@@ -152,4 +152,43 @@ class CartService extends BaseCartService
 
         $this->carts = array_values($Carts);
     }
+
+    public function removeProduct($ProductClass, $options = [])
+    {
+        $removeItem = $options['cart_item_data'] ?? null;
+
+        if (null === $removeItem) {
+            // カートアイテムデータが渡されていない場合は、商品規格のみで削除を試みる
+            $cartItem = new CartItem();
+            $cartItem->setProductClass($ProductClass);
+
+            if (!$ProductClass instanceof ProductClass) {
+                $ProductClassId = $ProductClass;
+                $ProductClass = $this->entityManager
+                    ->getRepository(ProductClass::class)
+                    ->find($ProductClassId);
+                if (is_null($ProductClass)) {
+                    return false;
+                }
+            }
+
+            $removeItem = new CartItem();
+            $removeItem->setPrice($ProductClass->getPrice02IncTax());
+            $removeItem->setProductClass($ProductClass);
+        }
+
+        $allCartItems = $this->mergeAllCartItems();
+        $foundIndex = -1;
+        foreach ($allCartItems as $index => $itemInCart) {
+            if ($this->cartItemComparator->compare($itemInCart, $removeItem)) {
+                $foundIndex = $index;
+                break;
+            }
+        }
+
+        array_splice($allCartItems, $foundIndex, 1);
+        $this->restoreCarts($allCartItems);
+
+        return true;
+    }
 }
