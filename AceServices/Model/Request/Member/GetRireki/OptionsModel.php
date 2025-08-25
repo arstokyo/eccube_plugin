89123d6d6f95b1f6a5f1a6d3e86ea675eb01fe35
ArsPhuoc <m.phuoc.le@ar-system.co.jp>
2025-08-25 19:05:35 +0900

    `GetRireki` モデル用 `Prm` オプション追加 * 新規 `RirekiModel` 作成（`BaseRirekiModel` 継承） — 新しいプロパティとメソッドを追加 * `CustomerDataConverter` を更新し、`GetRireki` 要求の構築ロジックを追加（`IdPrmModel` の生成とオプション設定を含む） * `IdPrmModel`、`OptionsModel` とそれらのインターフェースを追加してサポートを実装 * `GetRirekiRequestModel` を改良し、新パラメータ処理とリクエスト整合性の担保を実施

---
+++ AceServices/Model/Request/Member/GetRireki/OptionsModel.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\AceClient43\AceServices\Model\Request\Member\GetRireki;
+
+/**
+ * Model for Options
+ */
+class OptionsModel implements OptionsModelInterface
+{
+    private ?string $returnJdFreeKubuns = null;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getReturnJdFreeKubuns(): ?string
+    {
+        return $this->returnJdFreeKubuns;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setReturnJdFreeKubuns(?array $returnJdFreeKubuns): self
+    {
+        if (is_array($returnJdFreeKubuns)) {
+            $this->returnJdFreeKubuns = implode(',', $returnJdFreeKubuns);
+        } else {
+            $this->returnJdFreeKubuns = '';
+        }
+
+        return $this;
+    }
+}
---
+++ AceServices/Model/Request/Member/GetRireki/OptionsModelInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\AceClient43\AceServices\Model\Request\Member\GetRireki;
+
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+interface OptionsModelInterface
+{
+    /**
+     * Get ReturnJdFreeKubuns
+     *
+     * @return string
+     */
+    public function getReturnJdFreeKubuns(): ?string;
+
+    /**
+     * Set ReturnJdFreeKubuns
+     *
+     * @param array $returnJdFreeKubuns
+     *
+     * @SerializedName("return_jdfree_kubuns")
+     *
+     * @return self
+     */
+    public function setReturnJdFreeKubuns(?array $returnJdFreeKubuns): self;
+}
