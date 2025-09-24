b3bccc9b84469c5ed5080e1066aab863fd8fa953
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-09-24 14:43:13 +0900

    MasterBridgeにgetFreeCodeV1のEndpointを追加

--- Bridge/MasterBridge.php
+++ Bridge/MasterBridge.php
@@ -14,18 +14,25 @@
 namespace Plugin\AceClient43\Bridge;
 
 use Plugin\AceClient43\AceServices\AceMethod\Master\GetFreeCdWithNameMethod;
+use Plugin\AceClient43\AceServices\AceMethod\WebApi\Master\V1\V1GetFreeCodeMethod;
 use Plugin\AceClient43\AceServices\Model\Request\Master\GetFreeCdWithName as RequestGetFreeCdWithName;
+use Plugin\AceClient43\AceServices\Model\Request\WebApi\Master\V1\GetFreeCode as RequestV1GetFreeCode;
 use Plugin\AceClient43\AceServices\Model\Response\Master\GetFreeCdWithName as ResponseGetFreeCdWithName;
+use Plugin\AceClient43\AceServices\Model\Response\WebApi\Master\V1\GetFreeCode as ResponseV1GetFreeCode;
 use Plugin\AceClient43\Exception\MissingRequestParameterException;
 
 class MasterBridge extends BaseBridge
 {
-    private GetFreeCdWithNameMethod $getFreeCdWithNameMethod;
+    protected GetFreeCdWithNameMethod $getFreeCdWithNameMethod;
+
+    protected V1GetFreeCodeMethod $v1GetFreeCodeMethod;
 
     public function __construct(
         GetFreeCdWithNameMethod $getFreeCdWithNameMethod,
+        V1GetFreeCodeMethod $v1GetFreeCodeMethod,
     ) {
         $this->getFreeCdWithNameMethod = $getFreeCdWithNameMethod;
+        $this->v1GetFreeCodeMethod = $v1GetFreeCodeMethod;
     }
 
     /**
@@ -72,6 +79,49 @@ class MasterBridge extends BaseBridge
                 ['code' => $code, 'syid' => $this->getSyid()]
             );
             throw $e;
+        }
+    }
+
+    /**
+     * 名称付きのフリーコードを取得する
+     *
+     * @param int $kubun
+     * @param array $codes
+     *
+     * @return ResponseV1GetFreeCode\V1GetFreeCodeItemModel[]|null
+     *
+     * @throws \RuntimeException
+     */
+    public function getFreeCodeV1(int $kubun, array $codes): ?array
+    {
+        /** @var RequestV1GetFreeCode\V1GetFreeCodeRequestModelInterface $requestModel */
+        $requestModel = $this->createRequestModel(RequestV1GetFreeCode\V1GetFreeCodeRequestModelInterface::class);
+
+        $request = $requestModel
+            ->setSyid($this->getSyid())
+            ->setKubun($kubun)
+            ->setCodes($codes)
+        ;
+
+        try {
+            $response = $this->v1GetFreeCodeMethod
+                ->withRequest($request)
+                ->send();
+
+            if (!$response->isOk()) {
+                throw new \RuntimeException('名称付きのフリーコードの取得に失敗しました: '.$response->getStatusCode());
+            }
+
+            /** @var ResponseV1GetFreeCode\V1GetFreeCodeResponseModel $responseObject */
+            $responseObject = $response->getResponse();
+
+            return $responseObject->getItems();
+        } catch (\Throwable $e) {
+            $this->logger->error(
+                '名称付きのフリーコードの取得に失敗しました: '.$e->getMessage(),
+                ['kubun' => $kubun, 'codes' => $codes, 'syid' => $this->getSyid()]
+            );
+            throw new \RuntimeException('名称付きのフリーコードの取得に失敗しました: '.$e->getMessage());
         }
     }
 }
