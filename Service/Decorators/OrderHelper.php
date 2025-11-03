fba5f5df0877c9f854b551d9586610b33d57b6eb
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-11-03 16:11:21 +0900

    税表示区分取得ロジックを追加
    ---
    
    - `OrderHelper` に新メソッド `getTaxDisplayType` を実装。
    - 税表示区分を商品種別に応じて取得できるように変更。
    - Ace側の価格は税込として扱うロジックを追加。
    
    これにより、商品種別ごとに適切な税表示区分を用いた処理が可能になる。

--- Service/Decorators/OrderHelper.php
+++ Service/Decorators/OrderHelper.php
@@ -21,7 +21,9 @@ use Eccube\Entity\Cart;
 use Eccube\Entity\CartItem;
 use Eccube\Entity\Customer;
 use Eccube\Entity\Master\DeviceType;
+use Eccube\Entity\Master\OrderItemType;
 use Eccube\Entity\Master\OrderStatus;
+use Eccube\Entity\Master\TaxDisplayType;
 use Eccube\Entity\Order;
 use Eccube\Entity\OrderItem;
 use Eccube\Entity\Shipping;
@@ -245,5 +247,28 @@ class OrderHelper extends BaseOrderHelper
         log_info('カートに商品が入っていません。');
 
         return false;
+    }
+
+    /**
+     * 税表示区分を取得する.
+     *
+     * - 商品: 税込
+     *
+     * @param $OrderItemType
+     *
+     * @return TaxDisplayType
+     */
+    public function getTaxDisplayType($OrderItemType)
+    {
+        if ($OrderItemType instanceof OrderItemType) {
+            $OrderItemType = $OrderItemType->getId();
+        }
+
+        // デフォルト通販Ace側の価格は税込で採用します。
+        if ($OrderItemType === OrderItemType::PRODUCT) {
+            return $this->entityManager->find(TaxDisplayType::class, TaxDisplayType::INCLUDED);
+        }
+
+        return parent::getTaxDisplayType($OrderItemType);
     }
 }
