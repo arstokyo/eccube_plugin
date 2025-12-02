29696cefe92fd8496d1387b9cbe5dd5d43606f5c
yash-j <yash-j@ar-system.co.jp>
2025-12-02 18:48:10 +0900

    FormType からラベルを削除し、order route ID を使用しました。ラベルのレベルを変更し、modify 行を削除しました。

--- Form/Type/Admin/ConfigType.php
+++ Form/Type/Admin/ConfigType.php
@@ -47,31 +47,26 @@ class ConfigType extends AbstractType
                     ],
                 ])
                 ->add('order_route_id', IntegerType::class, [
-                    'label' => '注文ルートID',
                     'required' => false,
                 ])
-                ->add('use_ace_delivery', ToggleSwitchType::class, ['label' => '配送手数料連携'])
-                ->add('use_ace_discount', ToggleSwitchType::class, ['label' => '割引連携'])
-                ->add('use_ace_charge', ToggleSwitchType::class, ['label' => '手数料連携'])
+                ->add('use_ace_delivery', ToggleSwitchType::class)
+                ->add('use_ace_discount', ToggleSwitchType::class)
+                ->add('use_ace_charge', ToggleSwitchType::class)
 
-                ->add('add_point_from_ace', ToggleSwitchType::class, [
-                    'label' => 'ACEポイント反映',
-                ])
-                ->add('enable_order_support_cart', ToggleSwitchType::class, ['label' => '受注サポート(カート)'])
-                ->add('enable_order_support_check_out', ToggleSwitchType::class, ['label' => '受注サポート(購入)'])
-                ->add('add_cart_index', ToggleSwitchType::class, ['label' => 'カート追加連携(一覧)'])
-                ->add('add_cart_shopping', ToggleSwitchType::class, ['label' => 'カート追加連携(購入)'])
-                ->add('validate_duplicate_entry', ToggleSwitchType::class, ['label' => '会員重複チェック(フロント)'])
-                ->add('validate_duplicate_admin_entry', ToggleSwitchType::class, ['label' => '会員重複チェック(管理画面)'])
-                ->add('redirect_forgot', ToggleSwitchType::class, ['label' => 'パスワード忘れリダイレクト'])
+                ->add('add_point_from_ace', ToggleSwitchType::class)
+                ->add('enable_order_support_cart', ToggleSwitchType::class)
+                ->add('enable_order_support_check_out', ToggleSwitchType::class)
+                ->add('add_cart_index', ToggleSwitchType::class)
+                ->add('add_cart_shopping', ToggleSwitchType::class)
+                ->add('validate_duplicate_entry', ToggleSwitchType::class)
+                ->add('validate_duplicate_admin_entry', ToggleSwitchType::class)
+                ->add('redirect_forgot', ToggleSwitchType::class)
                 ->add('forgot_path', TextType::class, [
-                    'label' => 'リダイレクト先パス',
                     'required' => false,
                     'empty_data' => '',
                 ])
-                ->add('default_payment_id', IntegerType::class, ['label' => 'デフォルト決済ID'])
+                ->add('default_payment_id', IntegerType::class)
                 ->add('default_transaction_type', ChoiceType::class, [
-                    'label' => 'デフォルト取引区分',
                     'choices' => [
                         '都度決済' => TransactionType::SINGLE_PAYMENT,
                         '掛け払い' => TransactionType::CREDIT_PAYMENT,
